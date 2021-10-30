2ba64ef6233910e99ee90b3a2ed948627fa24bf9
sagar24263 <sagar24263@gmail.com>
2021-10-30 15:30:34 +0530

    main func added

--- http_server.py
+++ http_server.py
@@ -82,10 +82,10 @@ class Shortner(BaseHTTPRequestHandler):
             self.wfile.write("Couldn't find {}".format(longuri).encode())
 
 
-        
-        
-port = int(os.environ.get('PORT', 8000))
 
-server_addr = ('',port)
-httpob = HTTPServer(server_addr,Shortner)
-httpob.serve_forever()
+if __name__ == '__main__':      
+    port = int(os.environ.get('PORT', 8000))
+
+    server_addr = ('',port)
+    httpob = HTTPServer(server_addr,Shortner)
+    httpob.serve_forever()
